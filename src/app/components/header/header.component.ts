3907054b81c258ba16e73d2c3e4e73e16d38d7a0
COAB1 <carol.o.ag@gmail.com>
2021-04-13 19:04:57 -0300

    Generate users component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,16 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-  <nav class="navbar is-dark">
+    <nav class="navbar is-dark">
 
-  <!-- logo -->
-  <div class="navbar-brand">
-    <a class="navbar-item">
-      <img src="assets/img/angular-logo.png">
-    </a>
-  </div>
-</nav>
-`,
+    <!-- logo -->
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        <img src="assets/img/angular-logo.png">
+      </a>
+    </div>
+
+    <!-- menu -->
+    <div class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" routerLink="">Home</a>
+        <a class="navbar-item" routerLink="contact">Contact</a>
+      </div>
+    </div>
+    
+  </nav>
+  `,
   styles: [
   ]
 })
