c16f9269e251ba720d528f70ea24275e58da3f04
COAB1 <carol.o.ag@gmail.com>
2021-04-13 22:47:09 -0300

    Call github api from userservice

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  apiUrl = 'https://api.github.com/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+    return this.http.get(`${this.apiUrl}?per_page=10`);
+  }
 }
